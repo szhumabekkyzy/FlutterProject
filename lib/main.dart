8f59430ddc722ee592aebc6836f99e7c81d35f83
szhumabekkyzy <sholpan.zhumabekkyzy@tele2.kz>
2025-12-11 18:38:19 +0500

    homework_12

--- lib/main.dart
+++ lib/main.dart
@@ -9,41 +9,94 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: CounterPage());
+    return MaterialApp(home: LoginPage());
   }
 }
 
-class CounterPage extends StatefulWidget {
-  const CounterPage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  State<CounterPage> createState() => _CounterPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _CounterPageState extends State<CounterPage> {
-  int counter = 0;
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Счётчик")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Счётчик: $counter', style: TextStyle(fontSize: 32)),
+      appBar: AppBar(title: Text("Форма входа")),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Введите email";
+                  }
+                  if (!value.contains("@")) {
+                    return "Email должен содержать @";
+                  }
+                  return null;
+                },
+              ),
 
-            SizedBox(height: 20),
+              SizedBox(height: 20),
 
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  counter++;
-                });
-              },
-              child: Text("Увеличить"),
-            ),
-          ],
+              TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Пароль",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Введите пароль";
+                  }
+                  if (value.length < 6) {
+                    return "Минимум 6 символов";
+                  }
+                  return null;
+                },
+              ),
+
+              SizedBox(height: 20),
+
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(
+                      context,
+                    ).showSnackBar(SnackBar(content: Text("Успешный вход!")));
+
+                    emailController.clear();
+                    passwordController.clear();
+                  }
+                },
+                child: Text("Войти"),
+              ),
+            ],
+          ),
         ),
       ),
     );
