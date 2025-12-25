25420de3c0f229a35b3333178145b540c5cf57fc
szhumabekkyzy <sholpan.zhumabekkyzy@tele2.kz>
2025-12-25 13:46:44 +0500

    homewrok_15

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,101 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
+/// ---------- MODEL ----------
+class Post {
+  final int id;
+  final String title;
+
+  Post({required this.id, required this.title});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(id: json['id'], title: json['title']);
+  }
+}
+
+/// ---------- API ----------
+Future<List<Post>> fetchPosts() async {
+  try {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+
+    if (response.statusCode == 200) {
+      final List data = jsonDecode(response.body);
+      return data.map((json) => Post.fromJson(json)).toList();
+    } else {
+      throw Exception('Ошибка сервера');
+    }
+  } catch (e) {
+    throw Exception('Ошибка загрузки');
+  }
+}
+
+/// ---------- APP ----------
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: TasksListScreen());
+    return const MaterialApp(home: PostsPage());
   }
 }
 
-class TasksListScreen extends StatelessWidget {
-  const TasksListScreen({super.key});
+/// ---------- PAGE ----------
+class PostsPage extends StatefulWidget {
+  const PostsPage({super.key});
 
-  final List<String> tasks = const ['Задача 1', 'Задача 2', 'Задача 3'];
+  @override
+  State<PostsPage> createState() => _PostsPageState();
+}
+
+class _PostsPageState extends State<PostsPage> {
+  List<Post> posts = [];
+  bool isLoading = true;
+  String? errorMessage;
+
+  @override
+  void initState() {
+    super.initState();
+    loadPosts();
+  }
+
+  Future<void> loadPosts() async {
+    try {
+      final result = await fetchPosts();
+      setState(() {
+        posts = result;
+        isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        errorMessage = 'Ошибка загрузки';
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Список задач')),
-      body: ListView.separated(
-        itemCount: tasks.length,
-
-        separatorBuilder: (context, index) {
-          return const Divider();
-        },
-
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: const Icon(Icons.check_box_outline_blank),
-            title: Text(tasks[index]),
-            onTap: () {
-              // Вариант 1: print
-              print(tasks[index]);
-
-              // Вариант 2: SnackBar
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text('Вы выбрали: ${tasks[index]}')),
-              );
-            },
-          );
-        },
+      appBar: AppBar(title: const Text('Посты')),
+      body: Center(
+        child: isLoading
+            ? const CircularProgressIndicator()
+            : errorMessage != null
+            ? Text(errorMessage!)
+            : ListView.builder(
+                itemCount: posts.length,
+                itemBuilder: (context, index) {
+                  final post = posts[index];
+                  return ListTile(title: Text(post.title));
+                },
+              ),
       ),
     );
   }
